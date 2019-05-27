c3460dada98f80f94becb0030f71a1b5f63e82e3
甘霖 <452392920@qq.com>
2019-05-27 16:40:53 +0800

    adjustment project

---
+++ web/build/index.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+module.exports = isProd => {
+    let rules
+    let plugins 
+
+    if (isProd) {
+        rules = [{
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }]
+        }, {
+            test: /\.s[c|a]ss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
+    } else {
+        plugins = [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+
+        rules = [{
+            test: /\.css$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }]
+        }, {
+            test: /\.s[c|a]ss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
+    }
+
+    return {rules, plugins}
+}
