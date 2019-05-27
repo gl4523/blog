d33bf8c052ce9b9e4845bda8ec2a4b42f41c37e7
甘霖 <452392920@qq.com>
2019-05-27 15:01:45 +0800

    add style

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import './style/github-markdown.css'
+import './style/github-markdown.css'
 const root = document.querySelector('#app')
 
 ReactDOM.render(<App />, root)
