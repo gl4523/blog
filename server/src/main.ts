0455d28ef0af59db40bb185128e9f0149cfe52fa
gl <452392920@qq.com>
2019-06-04 21:01:04 +0800

    add static server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,20 @@
 import Koa = require('koa')
 import http = require('http')
 import {connect} from 'mongoose'
+const {join} = require('path')
 const Logger = require('koa-logger')
 const Body = require('koa-body')
 const Cors = require('koa2-cors')
 const {promisify} = require('util')
 const bindRoutes = require('./modules')
+const Static = require('koa-static')
 import {ServerPort, DBUrl} from './const'
 
 const app = new Koa()
 const server = http.createServer(app.callback())
 
 app.use(Logger())
+app.use(Static(join(__dirname, '..', 'static')))
 app.use(Body())
 app.use(Cors())
 
