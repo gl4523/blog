c37b67e9e07b40e45110622e0a07afe56db67c7b
gl <452392920@qq.com>
2019-06-11 21:49:40 +0800

    add verify module

---
+++ server/src/modules/verify/index.ts
@@ -0,0 +1,9 @@
+import KoaRouter from 'koa-router'
+
+const router = new KoaRouter()
+
+router.get('.well-known/pki-validation/fileauth.txt', async (ctx, next) => {
+  ctx.body = "201906100000002fhzdvo9khzzynia2a1rkctv6k5zp1slhlqumkinau2unmzwcy"
+})
+
+export default router
