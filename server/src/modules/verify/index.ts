7b27a311a1745fd47fefe955fa3555218bd3cebc
gl <452392920@qq.com>
2019-06-11 22:00:13 +0800

    update

--- server/src/modules/verify/index.ts
+++ server/src/modules/verify/index.ts
@@ -1,8 +1,7 @@
 import KoaRouter from 'koa-router'
 
 const router = new KoaRouter()
-
-router.get('.well-known/pki-validation/fileauth.txt', async (ctx, next) => {
+router.get('/.well-known/pki-validation/fileauth.txt', async (ctx, next) => {
   ctx.body = "201906100000002fhzdvo9khzzynia2a1rkctv6k5zp1slhlqumkinau2unmzwcy"
 })
 
