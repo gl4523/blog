c9b50eb30e6c07118cfc996a2cf59a924f30aaae
gl <452392920@qq.com>
2019-06-11 21:54:27 +0800

    fixed: router instance

--- server/src/modules/verify/index.ts
+++ server/src/modules/verify/index.ts
@@ -6,4 +6,4 @@ router.get('.well-known/pki-validation/fileauth.txt', async (ctx, next) => {
   ctx.body = "201906100000002fhzdvo9khzzynia2a1rkctv6k5zp1slhlqumkinau2unmzwcy"
 })
 
-export default router
+module.exports = router
