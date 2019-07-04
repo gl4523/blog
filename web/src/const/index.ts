93cdccc4dc7e14ee4f0a2150c9aae1a001c2f125
gl <452392920@qq.com>
2019-07-04 21:31:44 +0800

    update

--- web/src/const/index.ts
+++ web/src/const/index.ts
@@ -1,6 +1,6 @@
 let ServerUrl
 if (process.env.NODE_ENV === "production") {
-    ServerUrl = "https://gump.fun"
+    ServerUrl = "https://193.112.11.35"
 } else {
     ServerUrl = "http://127.0.0.1:3000"
 }
