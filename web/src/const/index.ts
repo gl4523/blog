90e5b03294d9af8240ec19582b49aedf51e139f8
gl <452392920@qq.com>
2019-07-25 22:24:47 +0800

    update static

--- web/src/const/index.ts
+++ web/src/const/index.ts
@@ -1,6 +1,6 @@
 let ServerUrl
 if (process.env.NODE_ENV === "production") {
-    ServerUrl = "https://193.112.11.35"
+    ServerUrl = "https://gl15.xyz"
 } else {
     ServerUrl = "http://127.0.0.1:3000"
 }
